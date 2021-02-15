57eeaf3f845b50e4ae4729edf9f5bf3444bd39e2
Will Sherrer <wsherre@clemson.edu>
2021-02-14 22:24:53 -0500

    asdf

--- libmythreads.c
+++ libmythreads.c
@@ -14,9 +14,9 @@ extern int threadCreate(thFuncPtr funcPtr, void *argPtr){
     pthread_t thread;
     pthread_attr_t *attr;
 
-    /*//all of this could be completely wrong on how to set a stacksize for a thread idk
+    //all of this could be completely wrong on how to set a stacksize for a thread idk
     pthread_attr_init(attr);
-    pthread_attr_setstacksize (attr, STACK_SIZE);
+    /*pthread_attr_setstacksize (attr, STACK_SIZE);
 
     if(pthread_create(&thread, attr, funcPtr, argPtr) == 0){
         return 0;
