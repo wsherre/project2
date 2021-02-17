bc6841592a5243e76d36232c8a2f9d5f136a5328
Will Sherrer <wsherre@clemson.edu>
2021-02-16 21:49:08 -0500

    sadf

--- libmythreads.c
+++ libmythreads.c
@@ -19,6 +19,7 @@ extern int threadCreate(thFuncPtr funcPtr, void *argPtr){
     newcontext.uc_stack.ss_size = STACK_SIZE ;
     newcontext.uc_stack.ss_flags = 0;
     makecontext(&newcontext, funcPtr, 1, argPtr);
+    t(argPtr);
 
 
     printf("hey\n");
