aee9a0a6ee73f39e913d5453b4b8718a24b8d21d
Will Sherrer <wsherre@clemson.edu>
2021-02-25 15:02:43 -0500

    adsf

--- libmythreads.c
+++ libmythreads.c
@@ -128,6 +128,7 @@ extern int threadCreate(thFuncPtr funcPtr, void *argPtr){
     thread_lib[thread_lib_size].thread_context = newcontext;
     thread_lib[thread_lib_size].active = true;
     thread_lib_size++;
+    printf("malloc: %d\n", thread_lib_size);
     active_threads++;
     
 
@@ -205,6 +206,7 @@ extern void threadExit(void *result){
     exited_lib[current_running_tid] = result;
 
     //nothing is ever free appparently. not even meaningless bytes of data
+    printf("malloc: %d\n", current_running_tid);
     free(thread_lib[current_running_tid].thread_context.uc_stack.ss_sp);
     
     //unactivate and set isExited to true for reasons mentioned in threadJoin
