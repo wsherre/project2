d4dd731ee06d740593fe58aabf295657542d0c0a
Will Sherrer <wsherre@clemson.edu>
2021-02-28 02:17:18 -0500

    df

--- libmythreads.c
+++ libmythreads.c
@@ -84,7 +84,7 @@ extern void threadInit(){
 
 void library_resize(){
     array_size *= 2;
-    thread_lib = realloc(thread_lib, (array_size + 1) * sizeof(library));
+    thread_lib = realloc(thread_lib, (array_size + 2) * sizeof(library));
     exited_lib = realloc(exited_lib, array_size * sizeof(void *));
     for(int i = thread_lib_size; i < array_size; ++i){
         thread_lib[i].active = false;
