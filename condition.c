060852372da3c8171971effce9427242d3092acf
Will Sherrer <wsherre@clemson.edu>
2021-03-01 22:33:40 -0500

    asdf

--- condition.c
+++ condition.c
@@ -13,20 +13,22 @@ void *t1(void*arg){
     threadYield();
     threadSignal(0, 5);
     threadSignal(0,5);
+    threadSignal(0, 5);
+    threadSignal(0, 5);
     return NULL;
 }
 
 int main(){
 
 
-  int id1, id2, id3;
-  int p1;
-  int p2;
+  int id1, id2, id3, id4, id5;
+  int p1, p2, p3, p4;
 
   int array[3000];
 
   p1 = 0;
   p2 = 5; 
+
 
   int *result1, *result2;
 
@@ -35,7 +37,9 @@ int main(){
 
   id1 = threadCreate(t, (void*)&p1);
   id2 = threadCreate(t, (void*)&p2);
-  id3 = threadCreate(t1, (void*)&p1);
+  id3 = threadCreate(t, (void*)&p3);
+  id4 = threadCreate(t, (void*)&p4);
+  id5 = threadCreate(t1, (void*)&p1);
 
   threadJoin(id1, (void *)&result1);
   printf("joined #1 --> %d.\n", *result1);
@@ -45,5 +49,11 @@ int main(){
 
   threadJoin(id3, (void *)&result1);
   printf("joined #3 --> %d.\n", *result1);
+
+  threadJoin(id4, (void *)&result1);
+  printf("joined #4 --> %d.\n", *result1);
+
+  threadJoin(id3, (void *)&result1);
+  printf("joined #4 --> %d.\n", *result1);
   
 }
